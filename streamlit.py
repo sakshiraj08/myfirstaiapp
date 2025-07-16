41ecd02e1581f139eb53f82238277b4686ff38ac
Sakshi Raj <sakshiraj2404@gmail.com>
2025-07-16 20:20:55 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,36 @@
+# pip install --upgrade langchain langchain-google-genai streamlit
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain import LLMChain
+from langchain import PromptTemplate
+from langchain import LLMChain
+from langchain import PromptTemplate
+
+import os
+
+os.environ['GOOGLE_API_KEY'] = "AIzaSyAxa5JTxKbSt1u-WcOcIrdYX5HqNccsPwU"
+
+# Create prompt template for generating tweets
+tweet_template = "Give me {number} tweets on {topic}"
+
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
+
+# Initialize Google's Gemini model
+gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
+
+# Create LLM chain using the prompt template and model
+tweet_chain = tweet_prompt | gemini_model
+
+# Example of using the LLM chain
+response = tweet_chain.invoke({"number": 5, "topic": "Wars in Middle East"}) 
+
+
+import streamlit as st
+st.header("Tweet Generator - Sakshi")
+st.subheader("Generate tweets using Generative AI ")
+topic=st.text_input("Topic")
+number=st.number_input("Number of tweets", min_value=1, max_value=10, value=1,step=1)
+if st.button("Generate"):
+ tweets=tweet_chain.invoke({"number": number, "topic": topic})
+ st.write(tweets.content)
+
